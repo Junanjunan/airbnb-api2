c5e905ffd05a177687789797da320c58812f988c
unknown <jjj1305@hanmail.net>
2021-08-10 00:46:08 +0900

    lecture 2.6

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost"
+        )
+        # fields = ("username", "superhost")
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = (
@@ -13,5 +27,4 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
             "is_active",
             "date_joined",
-            "favs",
         )
