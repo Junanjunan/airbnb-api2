dcfe5b3f114300582c03ba68e6d245a8b5223901
unknown <jjj1305@hanmail.net>
2021-08-10 00:13:48 +0900

    lecture 2.5

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Room
+from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
-# Create your views here.
+
+class RoomsView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()[:5]
+        serializer = ReadRoomSerializer(rooms, many=True).data
+        return Response(serializer)
+    def post(self, request):
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        serializer = WriteRoomSerializer(data=request.data)
+        if serializer.is_valid():
+            room = serializer.save(user=request.user)
+            room_serializer = ReadRoomSerializer(room).data
+            return Response(data=room_serializer, status=status.HTTP_200_OK)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomView(APIView):
+
+    def get_room(self, pk):
+        try:
+            room = Room.objects.get(pk=pk)
+            return room
+        except Room.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            serializer = ReadRoomSerializer(room).data
+            return Response(serializer)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            serializer = WriteRoomSerializer(room, data=request.data, partial=True)     # room이 instance가 된다 - 이 room을 넣지 않으면 serializer는 update가 아닌 create하는 것으로 인지한다.  // create일때는 room 없이 data=request.data였음
+            if serializer.is_valid():
+                room = serializer.save()
+                return Response(ReadRoomSerializer(room).data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+            
+    def delete(self, request, pk):
+        room = self.get_room(pk)
+        if room.user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if room is not None:
+            room.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
