aef4932274a7afe1f10e6260920f0ff48cb1cb1a
unknown <jjj1305@hanmail.net>
2021-08-11 01:38:03 +0900

    lecture 3.0

--- users/serializers.py
+++ users/serializers.py
@@ -14,17 +14,31 @@ class RelatedUserSerializer(serializers.ModelSerializer):
         )
         # fields = ("username", "superhost")
 
+class UserSerializer(serializers.ModelSerializer):
 
-class ReadUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        exclude = (
-            "groups",
-            "user_permissions",
-            "password",
-            "last_login",
-            "is_superuser",
-            "is_staff",
-            "is_active",
-            "date_joined",
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "avatar",
+            "superhost",
+            "password"
         )
+        read_only_fields = (
+            "id",
+            "superhost",
+            "avatar"
+        )
+
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,26 +1,87 @@
-from users.serializers import ReadUserSerializer
+import jwt
+from django.contrib.auth import authenticate
+from django.conf import settings
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import ReadUserSerializer
+from .serializers import UserSerializer
 from .models import User
-
+from rooms.serializers import RoomSerializer
+from rooms.models import Room
 
 class MeView(APIView):
 
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         if request.user.is_authenticated:             # is_authenticated()라고 하면 'bool' object is not callable 라는 오류 발생
-            return Response(ReadUserSerializer(request.user).data)
+            return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = UserSerializer(request.user, data=request.data, partial=True)        # request.user: instance임
+        if serializer.is_valid():
+            serializer.save()
+            return Response(UserSerializer(request.user).data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
+        return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    def put(self, request):             # favs를 만드는 것이 아니라, 있는 favs를 수정해주는 것이니까 post가 아니라 put method
+        pk = request.data.get("pk", None)
+        user = request.user
+        if pk is not None:
+            try:
+                room = Room.objects.get(pk=pk)
+                if room in user.favs.all():
+                    user.favs.remove(room)
+                else:
+                    user.favs.add(room)
+                return Response()
+            except Room.DoesNotExist:
+                pass
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UsersView(APIView):
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user).data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        encoded_jwt = jwt.encode({"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256")
+        print(encoded_jwt)
+        return Response(data={"token": encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
